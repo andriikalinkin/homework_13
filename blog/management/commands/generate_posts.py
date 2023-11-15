653249c553e8f58398cebdbf9ef8fd58cb3ffdd8
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-11-15 20:57:37 +0100

    Add Django custom command

---
+++ blog/management/commands/generate_posts.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+
+from blog.models import Blog
+
+fake = Faker()
+
+
+class Command(BaseCommand):
+    help = "Add random posts to the database"
+
+    def add_arguments(self, parser):
+        parser.add_argument("number", type=int, help="Numbers of posts")
+
+    def handle(self, *args, **options):
+        count = options["number"]
+
+        for i in range(count):
+            post = Blog.objects.create(
+                title=fake.word().capitalize(),
+                content=fake.text(),
+            )
+
+            self.stdout.write(
+                self.style.SUCCESS(f"Successfully created posts! Post id: {post.id}")
+            )
